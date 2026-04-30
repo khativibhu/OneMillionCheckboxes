60311c8f387a7a8afe050beda4f466a02703c000
Vaibhav Khati <khativibhu@gmail.com>
2026-04-30 22:45:44 +0530

    added public files i.e. static files index.html and style.css

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import http from "node:http";
+import path from "node:path";
+
 import express from "express";
 
 async function main() {
  const app = express();
  const httpServer = http.createServer(app);
  const PORT = process.env.PORT ?? 8000;
+
+ app.use(express.static(path.resolve("./public")) );
 
  httpServer.listen(PORT, () => {
     console.log(`server is running on host machine http://localhost:${PORT}`);
