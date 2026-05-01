01af4bcf8a8b840a26c712fe8eac0be4d7c3087e
Vaibhav Khati <khativibhu@gmail.com>
2026-05-01 12:06:00 +0530

    emitting the client event on client side and registering listener on server side And emitted the server event on server side and registered its listener on client side

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ async function main() {
  //Socket IO handler
  io.on("connection",(socket)=>{
    console.log("Socket connected", {id: socket.id});
+
+   socket.on("client:checkbox:change",(data)=>{
+   console.log(`[Socket: ${socket.id}]: client:checkbox:change`,data);
+   io.emit("server:checkbox:change", data);
  });
+ 
+ });
+ 
 
 //Express handler
  app.use(express.static(path.resolve("./public")) );
