91a0526bab3507297ec6a56e33d76bfe7c1f1dc6
Vaibhav Khati <khativibhu@gmail.com>
2026-05-01 19:47:15 +0530

    basic redis our relay server setup

---
+++ redis-connection.js
@@ -0,0 +1,12 @@
+import Redis from "ioredis";
+
+export const publisher = new Redis({
+    host: localhost,
+    port: 6379,
+});
+
+export const subscriber = new Redis({
+    host: localhost,
+    port: 6379,
+});
+
